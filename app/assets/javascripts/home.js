798068cfa16aea847bc311a50680e839a9a2632f
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-01-12 14:58:37 +0900

    impl format radio

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,7 @@
 (function($){
     $(document).ready(function(){
         $("#run_query").click(function(){
-            var format = "json"
+            var format = get_radio_value("format-radio-group");
             $.ajax({
                 url: "/home/run_query." + format,
                 type: "POST",
@@ -15,9 +15,19 @@
                     $("#result").addClass(res.status);
 
                     if(format == "json"){
-                        $("#result").val( JSON.stringify(res.result));
+                        $("#result").val( JSON.stringify(res.result) );
                     }
                 });
         });
+
+        function get_radio_value(id){
+            var selected_value = null;
+            $("#" + id + " button").each(function(){
+                if($(this).hasClass("active")){
+                    selected_value = $(this).val();
+                }
+            });
+            return selected_value;
+        }
     });
 })(jQuery);
