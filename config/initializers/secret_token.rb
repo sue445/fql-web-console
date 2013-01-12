b4983892f84821bbf642a438b56a7a0bcca0a930
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-01-12 11:09:09 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FqlWebConsole::Application.config.secret_token = '7c2676477090f6ad395b7e3d8f468d12dbfc11dba07ea45b807845fd122689c250cdb72633c9b1adfc7a30bf3a94449dd742b79b9ebbd4d0a9f33664022a2a5b'
